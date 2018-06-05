c9bdde253cc861d4e0455ec3d4ea93bfeabdeca2
Indranil Chatterjee <indranil.cse97@gmail.com>
2018-06-05 11:15:26 +0530

    temp stuff

--- app/src/main/java/com/example/krishna/bukie/ChatActivity.java
+++ app/src/main/java/com/example/krishna/bukie/ChatActivity.java
@@ -1,7 +1,14 @@
 package com.example.krishna.bukie;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ActionMenuView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -9,5 +16,60 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+
+
+        //toolbar.setTitle("Hello");
+        //toolbar.setTitleTextColor(getResources().getColor(android.R.color.black));
+        setSupportActionBar(toolbar);
+        ActionMenuView amv = (ActionMenuView) toolbar.findViewById(R.id.amv);
+        amv.setOnMenuItemClickListener(new ActionMenuView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return onOptionsItemSelected(menuItem);
+            }
+        });
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+       //ActionBar ab=getSupportActionBar();
+       //ab.setIcon(R.drawable.boy);
+       //ab.setDisplayHomeAsUpEnabled(true);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main_actions, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView =
+                (SearchView) searchItem.getActionView();
+
+        // Configure the search info and add any event listeners...
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_refresh was selected
+            case R.id.action_search:
+                Toast.makeText(this, "search selected", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            // action with ID action_settings was selected
+
+            default:
+                break;
+        }
+        return true;
     }
 }
+
